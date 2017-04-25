c682f809918e9fac7de96c1059d660dc8d38fa74
johnr5850 <johnr5850@hstat.org>
2017-04-25 14:24:38 +0000

    original added

--- original.js
+++ original.js
@@ -1,23 +1,17 @@
-var ballX; //declares variables called ballX and ballY
-var ballY;
-var ballSpeedX;//sets variables ballSpeedX and Y equal to 5
-var ballSpeedY;
-var paddleHeight;//makes the original paddle height 30
-var paddleColor; 
-var transpColor;
-
-function setup() {
-  createCanvas(800, 400);//sets the canvas size to 800 by 600
+int ballX; //declares variables called ballX and ballY
+int ballY;
+int ballSpeedX = 5;//sets variables ballSpeedX and Y equal to 5
+int ballSpeedY = 5;
+int paddleHeight = 30;//makes the original paddle height 30
+int paddleColor; 
+int transpColor = 255;
+void setup() {
+  size(800, 400);//sets the canvas size to 800 by 600
   noCursor();//disables the cursor from showing up inside the canvas
   ballX = width/2;//when the code starts, the ball will be positioned in the middle
   ballY = height/2;//of the canvas
-  ballSpeedX = 5;
-  ballSpeedY = 5;
-  paddleHeight = 30;
-  transpColor = 255;
-}
-
-function draw() {  //this loop will run forever
+  }
+void draw() {  //this loop will run forever
   fill(0,0,0,transpColor);
   background(255);//sets the background color to grey
   moveBall();//runs function moveBall
@@ -25,14 +19,12 @@ function draw() {  //this loop will run forever
   drawPaddle();//draws the moveable paddle
   drawBall();//draws the ball
 }
-
-function moveBall() { 
+void moveBall() { 
   ballX = ballX + ballSpeedX;//changes the balls x and y positions
   ballY = ballY + ballSpeedY;//so that they can move
-  prvarln("ballSpeedX: " + ballSpeedX + ", ballSpeedY: " + ballSpeedY);//will prvar text in console //indicating the speed of the ball
+  println("ballSpeedX: " + ballSpeedX + ", ballSpeedY: " + ballSpeedY);//will print text in console indicating the speed of the ball
 }
-
-function checkCollide() {
+void checkCollide() {
   if (ballX < 0 || ballX > width) { //checks if the ball hits the side of the canvas
     ballSpeedX = ballSpeedX * -1;//will reverse the balls direction
   }
@@ -44,31 +36,25 @@ function checkCollide() {
       ballSpeedY = ballSpeedY * -1;//reverses the balls direction
       transpColor = transpColor - 40;
       transpColor = constrain(transpColor,5,255);
-    } 
-    else {//if the ball doesn’t hit the paddle
+    } else {//if the ball doesn’t hit the paddle
       text("GAME OVER!", width/2, height/2);//prints GAME OVER at the center of the canvas
       noLoop();//ends the code
     }
   }
 }
-
-function drawBall() {
+void drawBall() {
   noStroke();//disables outlines of the shape
   ellipse(ballX, ballY, 20, 20);//draws a circle
 }
-
-function drawPaddle() {
+void drawPaddle() {
   stroke(paddleColor);//the outlines color will be set to paddleColor’s value
   strokeWeight(4);//thickens the outline
-  rect(mouseX, height-paddleHeight-4, 200, paddleHeight);//draws a rectangle that follow the //mouse
+  rect(mouseX, height-paddleHeight-4, 200, paddleHeight);//draws a rectangle that follow the mouse
 }
-
-function mousePressed() {//when the mouse is pressed
-  paddleColor = (map(mouseX, 0, width, 0, 255));//changes the value of paddleColor based 
-//on the position of the mouse’s X coordinate
+void mousePressed() {//when the mouse is pressed
+  paddleColor = int((map(mouseX, 0, width, 0, 255)));//changes the value of paddleColor based on the position of the mouse’s X coordinate
 }
-
-function keyPressed() {//when the keys
+void keyPressed() {//when the keys
   if (key == 'b' || key == 'B') {//b or B is pressed
     fill(0);//fills the colors of the shape to black
   }
