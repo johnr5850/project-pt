4af6f2ec42d5f9c0fe358ef8e7a3580c24b38a8f
johnr5850 <johnr5850@hstat.org>
2017-04-28 14:11:22 +0000

    add timer

--- progress.js
+++ progress.js
@@ -5,14 +5,15 @@ int ballSpeedY = 5;
 int paddleHeight = 30;//makes the original paddle height 30
 int paddleColor; 
 int transpColor = 255;
-int autoX = 30;
-int autoY = 80;
+int time;
 
 void setup() {
+  frameRate(60);
   size(800, 400);//sets the canvas size to 800 by 600
   noCursor();//disables the cursor from showing up inside the canvas
   ballX = width/2;//when the code starts, the ball will be positioned in the middle
   ballY = height/2;//of the canvas
+  time = millis();
 }
 void draw() {  //this loop will run forever
   fill(0,0,0,transpColor);
@@ -21,6 +22,7 @@ void draw() {  //this loop will run forever
   checkCollide();//checks collision from the ball
   drawPaddle();//draws the moveable paddle
   drawBall();//draws the ball
+  
 }
 void moveBall() { 
   ballX = ballX + ballSpeedX;//changes the balls x and y positions
@@ -31,19 +33,19 @@ void checkCollide() {
   if (ballX < 0 || ballX > width) { //checks if the ball hits the side of the canvas
     ballSpeedX = ballSpeedX * -1;//will reverse the balls direction
   }
-  if (ballY < 0 || (ballY < autoY && ballX < autoX)) { //checks if the ball hits the top of the canvas + extra paddle from bottom
+  if (ballY < 0) { //checks if the ball hits the top of the canvas
     ballSpeedY = ballSpeedY * -1;//reverses the balls direction
   }
-  if (ballY < 80 && ballX) {
-  }
-  if (ballY > height-paddleHeight) { //checks if the ball 
-    if (ballX > mouseX && ballX < mouseX + 200) { //is hitting the paddle
+  if (ballY > 350) { //checks if the ball 
+    if (ballX > mouseX - 100 && ballX < mouseX + 100) { //is hitting the paddle
       ballSpeedY = ballSpeedY * -1;//reverses the balls direction
       transpColor = transpColor - 40; //decrements transparancy levels of the ball
       transpColor = constrain(transpColor,50,255);
       }
     else {//if the ball doesn’t hit the paddle
-      text("GAME OVER!", 375, 200);//prints GAME OVER at the center of the canvas
+      strokeWeight(4);
+      int timer = millis();
+      text("GAME OVER! Your Time Was: "+ (float(timer)/1000), 375, 200);//prints GAME OVER at the center of the canvas
       noLoop();//ends the code
     }
   }
@@ -55,9 +57,7 @@ void drawBall() {
 void drawPaddle() {
   stroke(paddleColor);//the outlines color will be set to paddleColor’s value
   strokeWeight(4);//thickens the outline
+  rectMode(CENTER);
   rect(mouseX, height-paddleHeight-4, 200, paddleHeight);//draws a rectangle that follow the mouse
-  rect(autoX,autoY,200,paddleHeight);
 }
-
-
 
